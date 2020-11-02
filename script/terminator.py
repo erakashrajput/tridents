70655c11aaf89e48534ef390bf361753fbb71d51
Akash Rajput <akash.rajput@bloomreach.com>
2020-11-03 01:34:56 +0530

    Added merge on prod and attribute: Working

--- script/terminator.py
+++ script/terminator.py
@@ -1,34 +1,31 @@
 import time
 import logging
+import os
 
-OUTPUT_FILE= "/Users/akash.rajput/Downloads/CUSTOMERS/automated/mscdirect/items/testing/out3.txt"
-OUTPUT_FILE1="/Users/akash.rajput/Downloads/CUSTOMERS/automated/mscdirect/items/testing/out2.txt"
-INPUT_FILE = "/Users/akash.rajput/Downloads/CUSTOMERS/automated/mscdirect/items/IT_Alternate.TXT"
-INPUT_FILE1 = "/Users/akash.rajput/Downloads/CUSTOMERS/automated/mscdirect/items/testing/testing.txt"
-
-LOG_FILENAME = 'example.log'
+LOG_FILENAME = 'debug.log'
+WORKING_DIR = os.getcwd()
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 PART_ID_ARR = []
-# SOURCE_FILES = ["IT_Alternate.TXT ","IT_Alternate1.TXT ","IT_Alternate2.TXT ",
-#                 "IT_Alternate3.TXT ","IT_Product1.TXT","IT_Product2.TXT","IT_Product3.TXT","IT_Product4.TXT"]
-SOURCE_FILES_TEST = ["IT_Alternate.TXT ","IT_Alternate1.TXT ","IT_Product1.TXT ","_Product2.TXT ",]
+SOURCE_FILES_ATTRIBUTE = ["IT_Alternate.TXT","IT_Alternate1.TXT"]
+SOURCE_FILES_PRODUCT = ["IT_Product1.TXT","IT_Product2.TXT"]
 
-def Repeat(x): 
-    _size = len(x) 
-    repeated = [] 
-    for i in range(_size): 
-        k = i + 1
-        for j in range(k, _size): 
-            if x[i] == x[j] and x[i] not in repeated: 
-                repeated.append(x[i]) 
-    return repeated 
+# def Repeat(x): 
+    # _size = len(x) 
+    # repeated = [] 
+    # for i in range(_size): 
+    #     k = i + 1
+    #     for j in range(k, _size): 
+    #         if x[i] == x[j] and x[i] not in repeated: 
+    #             repeated.append(x[i]) 
+    # return repeated 
   
-
-def terminator():
+def json_validator(key,val):
+    pass
+def record_creator(file_,output_file):
     entry_dict = dict()
     prod_key = ''
-    with open(INPUT_FILE1, "r", errors='replace') as input_file, open(OUTPUT_FILE1, "w") as output_file:
+    with open(file_, "r", errors='replace') as input_file:
 
         # IT WILL READ LINE BY LINE
         # HAVEN'T USED READLINE() TO AVAOID LOADING ENTIRE LIST IN RAM TO SAVE MEMORY
@@ -42,14 +39,19 @@ def terminator():
 
                 if line == "REC$$": # END OF PRODUCT RECORD
                     output_file.write("{}\t{}\n".format(prod_key, entry_dict))
-                    entry_dict = {}
+                    # entry_dict = {}
+                    entry_dict.clear()
                     prod_key = ""
 
                 else:
-                    record_row_key = line.split("=")[0]
-                    record_row_value = line.split("=")[1]
+                    record_row_key = line.split("=",1)[0]
+                    record_row_value = line.split("=",1)[1]
+                    if record_row_key is "":
+                        print("Issue encountered")
+                        continue
+
                     if record_row_key == "part":
-                        prod_key = "{}\t".format(record_row_value)
+                        prod_key = "{}".format(record_row_value)
                         PART_ID_ARR.append(prod_key)
                     else:
                         if record_row_key in entry_dict:
@@ -62,24 +64,39 @@ def terminator():
                 logging.debug("{}:{}".format(
                     time.time(), Exception.with_traceback))
 
-def merge_line_files():
-    pass
+def sort_file(line_file_name):
+    sort_string= "sort "+WORKING_DIR+"/output/"+line_file_name+ " -o "+WORKING_DIR+"/output/"+line_file_name
+    os.system(sort_string)
+
+def merge_master_line():
+    os.chdir(WORKING_DIR+"/output/")
+    # MADE LEFT OUTER JOIN
+    merge_string = "join -a1 prod.txt attribute.txt |sed 's/} {/, /' > merge_file.txt"
+    os.system(merge_string)
+
+def aggregated_line_files(files_array, line_file_name):
+    
+    with open(WORKING_DIR+"/output/" + line_file_name, "w") as output_file:
+
+            for file_ in files_array:
+                file_ = WORKING_DIR+"/items/"+file_   
+                record_creator(file_,output_file)
+    sort_file(line_file_name)
+
 def main():
-    start = time.time()
-    terminator()
-    end = time.time()
-    print(Repeat(PART_ID_ARR))
-    print("Time:{}".format(end-start))
+    # aggregated_line_files(SOURCE_FILES_ATTRIBUTE,"attribute.txt")
+    # aggregated_line_files(SOURCE_FILES_PRODUCT,"prod.txt")
+    merge_master_line()
     pass
-
-
+    
 if __name__ == "__main__":
+    start = time.time() 
     main()
+    end = time.time()
+    print("Time:{}".format(end-start))
 
-
-"""
-1. READ FILE -> EXTRACT DATA AND AND CLUB THEM TOGETHER
-2. WRTE DATA TO MASTER FILE
-3. 
-
-"""
+'''
+1. created line file for attri - sorted
+2. same for prod - sorted
+3. we will make a join on first
+'''

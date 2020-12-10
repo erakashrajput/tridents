aded786a6b0847e27e7bd144628046f3ac23ad30
Akash Rajput <akash.rajput@bloomreach.com>
2020-12-10 12:31:45 +0530

    taking the backup for Dec 10

--- preprocessor.sh
+++ preprocessor.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# echo "BASELINE FILE GENERATION STARTED: $(date)"
-# cat items/IT_[PA]*.TXT | iconv -f latin1 -t utf-8 > baseline.txt
+# for f in items/*
+# do
+#    echo "" >> $f
+# done
+echo "BASELINE FILE GENERATION STARTED: $(date)"
+cat items/IT_[PA]*.TXT | iconv -f latin1 -t utf-8 > baseline.txt
 
-# echo "BASE FILE GENERATION STARTED: $(date)"
-# python a.py baseline baseline.txt
+echo "BASE FILE GENERATION STARTED: $(date)"
+python nexla_record.py baseline baseline.txt
 
-# echo "LiveCad4Build.txt and ItemRestrictions.txt PROCESSING STARTED: $(date)"
-# python a.py other
+echo "LiveCad4Build.txt and ItemRestrictions.txt PROCESSING STARTED: $(date)"
+python nexla_record.py other
 
-# echo "MERGE LINE STARTED: $(date)"
-# sort base.txt LiveCad4Build.txt ItemRestrictions.txt > merge_line.txt
+echo "MERGE LINE STARTED: $(date)"
+sort base.txt LiveCad4Build.txt ItemRestrictions.txt > merge_line.txt
 
 echo "NEXLA FILE PROCESSING STARTED: $(date)"
-python a.py postprocess
+python nexla_record.py postprocess
 
 echo "SUCCESS"
